c84b110294f58b0a91c17729b63226e92213d66f
Maria Coelho <maeriacoelho@gmail.com>
2024-03-14 19:42:37 -0300

    Implementar a criação de jogador e o iniciar partida

--- magic-trick/Form1.Designer.cs
+++ magic-trick/Form1.Designer.cs
@@ -39,6 +39,20 @@
             this.label4 = new System.Windows.Forms.Label();
             this.lstPlayerList = new System.Windows.Forms.ListBox();
             this.label5 = new System.Windows.Forms.Label();
+            this.btnEnterMatch = new System.Windows.Forms.Button();
+            this.txtPlayerName = new System.Windows.Forms.TextBox();
+            this.txtPlayerPassword = new System.Windows.Forms.TextBox();
+            this.txtPlayerId = new System.Windows.Forms.TextBox();
+            this.btnStartMatch = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtFirstPlayer = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // lblVersion
@@ -55,9 +69,9 @@
             // 
             // btnCreateMatch
             // 
-            this.btnCreateMatch.Location = new System.Drawing.Point(82, 90);
+            this.btnCreateMatch.Location = new System.Drawing.Point(97, 122);
             this.btnCreateMatch.Name = "btnCreateMatch";
-            this.btnCreateMatch.Size = new System.Drawing.Size(241, 23);
+            this.btnCreateMatch.Size = new System.Drawing.Size(116, 23);
             this.btnCreateMatch.TabIndex = 1;
             this.btnCreateMatch.Text = "Criar partida";
             this.btnCreateMatch.UseVisualStyleBackColor = true;
@@ -65,17 +79,17 @@
             // 
             // txtMatchName
             // 
-            this.txtMatchName.Location = new System.Drawing.Point(82, 15);
+            this.txtMatchName.Location = new System.Drawing.Point(63, 19);
             this.txtMatchName.Name = "txtMatchName";
-            this.txtMatchName.Size = new System.Drawing.Size(241, 20);
+            this.txtMatchName.Size = new System.Drawing.Size(131, 20);
             this.txtMatchName.TabIndex = 2;
             // 
             // txtMatchPassword
             // 
-            this.txtMatchPassword.Location = new System.Drawing.Point(82, 52);
+            this.txtMatchPassword.Location = new System.Drawing.Point(63, 60);
             this.txtMatchPassword.Name = "txtMatchPassword";
             this.txtMatchPassword.PasswordChar = '*';
-            this.txtMatchPassword.Size = new System.Drawing.Size(241, 20);
+            this.txtMatchPassword.Size = new System.Drawing.Size(131, 20);
             this.txtMatchPassword.TabIndex = 3;
             // 
             // label1
@@ -83,7 +97,7 @@
             this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(31, 22);
+            this.label1.Location = new System.Drawing.Point(12, 26);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(35, 13);
             this.label1.TabIndex = 4;
@@ -94,7 +108,7 @@
             this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(31, 55);
+            this.label2.Location = new System.Drawing.Point(12, 63);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(38, 13);
             this.label2.TabIndex = 5;
@@ -107,7 +121,7 @@
             this.lstMatchList.FormattingEnabled = true;
             this.lstMatchList.Location = new System.Drawing.Point(34, 151);
             this.lstMatchList.Name = "lstMatchList";
-            this.lstMatchList.Size = new System.Drawing.Size(289, 264);
+            this.lstMatchList.Size = new System.Drawing.Size(179, 264);
             this.lstMatchList.TabIndex = 6;
             this.lstMatchList.SelectedIndexChanged += new System.EventHandler(this.LstMatchList_SelectedIndexChanged);
             // 
@@ -123,7 +137,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(365, 126);
+            this.label4.Location = new System.Drawing.Point(239, 126);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(56, 13);
             this.label4.TabIndex = 9;
@@ -134,9 +148,9 @@
             this.lstPlayerList.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left)));
             this.lstPlayerList.FormattingEnabled = true;
-            this.lstPlayerList.Location = new System.Drawing.Point(368, 151);
+            this.lstPlayerList.Location = new System.Drawing.Point(242, 151);
             this.lstPlayerList.Name = "lstPlayerList";
-            this.lstPlayerList.Size = new System.Drawing.Size(289, 264);
+            this.lstPlayerList.Size = new System.Drawing.Size(293, 264);
             this.lstPlayerList.TabIndex = 8;
             // 
             // label5
@@ -149,25 +163,152 @@
             this.label5.TabIndex = 10;
             this.label5.Text = "Amsterdã";
             // 
+            // btnEnterMatch
+            // 
+            this.btnEnterMatch.Location = new System.Drawing.Point(164, 53);
+            this.btnEnterMatch.Name = "btnEnterMatch";
+            this.btnEnterMatch.Size = new System.Drawing.Size(121, 23);
+            this.btnEnterMatch.TabIndex = 11;
+            this.btnEnterMatch.Text = "Entrar na partida";
+            this.btnEnterMatch.UseVisualStyleBackColor = true;
+            this.btnEnterMatch.Click += new System.EventHandler(this.btnEnterMatch_Click);
+            // 
+            // txtPlayerName
+            // 
+            this.txtPlayerName.Location = new System.Drawing.Point(47, 19);
+            this.txtPlayerName.Name = "txtPlayerName";
+            this.txtPlayerName.Size = new System.Drawing.Size(100, 20);
+            this.txtPlayerName.TabIndex = 12;
+            this.txtPlayerName.TextChanged += new System.EventHandler(this.txtPlayerName_TextChanged);
+            // 
+            // txtPlayerPassword
+            // 
+            this.txtPlayerPassword.Location = new System.Drawing.Point(47, 56);
+            this.txtPlayerPassword.Name = "txtPlayerPassword";
+            this.txtPlayerPassword.Size = new System.Drawing.Size(100, 20);
+            this.txtPlayerPassword.TabIndex = 13;
+            // 
+            // txtPlayerId
+            // 
+            this.txtPlayerId.Location = new System.Drawing.Point(185, 19);
+            this.txtPlayerId.Name = "txtPlayerId";
+            this.txtPlayerId.Size = new System.Drawing.Size(100, 20);
+            this.txtPlayerId.TabIndex = 14;
+            // 
+            // btnStartMatch
+            // 
+            this.btnStartMatch.Location = new System.Drawing.Point(554, 33);
+            this.btnStartMatch.Name = "btnStartMatch";
+            this.btnStartMatch.Size = new System.Drawing.Size(127, 23);
+            this.btnStartMatch.TabIndex = 15;
+            this.btnStartMatch.Text = "Iniciar partida";
+            this.btnStartMatch.UseVisualStyleBackColor = true;
+            this.btnStartMatch.Click += new System.EventHandler(this.btnStartMatch_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.btnEnterMatch);
+            this.groupBox1.Controls.Add(this.txtPlayerId);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.txtPlayerName);
+            this.groupBox1.Controls.Add(this.txtPlayerPassword);
+            this.groupBox1.Location = new System.Drawing.Point(242, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(293, 100);
+            this.groupBox1.TabIndex = 16;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Jogador";
+            // 
+            // label6
+            // 
+            this.label6.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(6, 26);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "Nome";
+            // 
+            // label7
+            // 
+            this.label7.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(6, 63);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(38, 13);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "Senha";
+            // 
+            // label8
+            // 
+            this.label8.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(161, 26);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(18, 13);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "ID";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.txtMatchName);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.txtMatchPassword);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Location = new System.Drawing.Point(34, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(200, 100);
+            this.groupBox2.TabIndex = 17;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Partida";
+            // 
+            // label9
+            // 
+            this.label9.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(551, 75);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(85, 13);
+            this.label9.TabIndex = 19;
+            this.label9.Text = "Primeiro Jogador";
+            // 
+            // txtFirstPlayer
+            // 
+            this.txtFirstPlayer.Location = new System.Drawing.Point(642, 72);
+            this.txtFirstPlayer.Name = "txtFirstPlayer";
+            this.txtFirstPlayer.Size = new System.Drawing.Size(121, 20);
+            this.txtFirstPlayer.TabIndex = 19;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtFirstPlayer);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnStartMatch);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.lstPlayerList);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.lstMatchList);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.txtMatchPassword);
-            this.Controls.Add(this.txtMatchName);
             this.Controls.Add(this.btnCreateMatch);
             this.Controls.Add(this.lblVersion);
             this.MinimumSize = new System.Drawing.Size(816, 489);
             this.Name = "Form1";
             this.Text = "MagicTrick";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -186,6 +327,18 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.ListBox lstPlayerList;
         private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnEnterMatch;
+        private System.Windows.Forms.TextBox txtPlayerName;
+        private System.Windows.Forms.TextBox txtPlayerPassword;
+        private System.Windows.Forms.TextBox txtPlayerId;
+        private System.Windows.Forms.Button btnStartMatch;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox txtFirstPlayer;
     }
 }
 
