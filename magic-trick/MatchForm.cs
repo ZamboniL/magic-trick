2a34e3c2b9ecd8821d093c9729419776b75ac72f
Maria Coelho <maeriacoelho@gmail.com>
2024-03-21 15:16:11 -0300

    criar listagem de mão

--- magic-trick/MatchForm.cs
+++ magic-trick/MatchForm.cs
@@ -7,14 +7,44 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MagicTrickServer;
 
 namespace MagicTrick
 {
     public partial class MatchForm : Form
     {
-        public MatchForm()
+        public MatchForm(int idPartida)
         {
             InitializeComponent();
+            string resultado = Jogo.ConsultarMao(idPartida);
+            string[] maos = SepararStringDeResultado(resultado);
+            string primeiraCarta = maos[0];
+            string primeiroId = primeiraCarta.Split(',')[0];
+            for(int i=0; i< maos.Length; i++)
+            {
+                string carta = maos[i];
+                if (carta.Contains(primeiroId))
+                {
+                    lstCartas1.Items.Add(carta);
+                }
+                else
+                {
+                    lstCartas2.Items.Add(carta);
+                }
+            }
+        }
+        private string[] SepararStringDeResultado(string str)
+        {
+            str.Replace("\r", "");
+            str = str.Substring(0, str.Length - 1);
+            return str.Split('\n');
+        }
+
+        private void btnJogarCarta_Click(object sender, EventArgs e)
+        {
+            string cartaSelecionada = lstCartas1.SelectedItem.ToString();
+            int posicaoCarta = Int32.Parse(cartaSelecionada.Split(',')[1]);
+
         }
     }
 }
